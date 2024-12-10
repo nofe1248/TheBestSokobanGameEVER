1ed2eff4b731cb13e1a933751b8703a4604a08b6
Zhige Chen <zhigec_cpp@outlook.com>
2024-12-10 12:32:35 +0800

    Settings GUI fix

--- core/src/main/java/io/github/nofe1248/gui/Settings.java
+++ core/src/main/java/io/github/nofe1248/gui/Settings.java
@@ -2,6 +2,9 @@ package io.github.nofe1248.gui;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.ui.Cell;
+import com.badlogic.gdx.scenes.scene2d.ui.Slider;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 
@@ -13,6 +16,24 @@ public class Settings extends BaseGUI {
     @Override
     public void create() {
         super.create();
+
+        Table settingsTable = this.stage.getRoot().findActor("Info");
+        assert settingsTable != null;
+        Slider masterVolumeSlider = new Slider(0, 1, 0.01f, false, this.skin);
+        Slider musicVolumeSlider = new Slider(0, 1, 0.01f, false, this.skin);
+        Slider soundEffectVolumeSlider = new Slider(0, 1, 0.01f, false, this.skin);
+
+        var masterVolumeCell = settingsTable.getCells().get(4);
+        var musicVolumeCell = settingsTable.getCells().get(5);
+        var soundEffectVolumeCell = settingsTable.getCells().get(6);
+
+        assert masterVolumeCell != null;
+        assert musicVolumeCell != null;
+        assert soundEffectVolumeCell != null;
+
+        masterVolumeCell.padTop(12.8f).setActor(masterVolumeSlider);
+        musicVolumeCell.padTop(12.8f).setActor(musicVolumeSlider);
+        soundEffectVolumeCell.padTop(12.8f).setActor(soundEffectVolumeSlider);
 
         TextButton quitButton = this.stage.getRoot().findActor("quit");
         assert quitButton != null;
